731fd18932260c2df506bf8ecf2fbb78ba5585b5
henri <henri@HENRIQUE>
2024-04-18 23:04:55 -0300

    Add project files.

---
+++ SequentialStructureExercise2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace SequentialStrucutureExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14159;
+            double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = pi * radius * radius;
+
+            Console.WriteLine("A=" + result.ToString("F4"));
+        }
+    }
+}
